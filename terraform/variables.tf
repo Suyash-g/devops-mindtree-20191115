15deae3b80821e58b0d3aafed7f30c9ee961ca93
Radhika <radhikaswamy34@gmail.com>
2019-11-27 16:40:23 +0000

    commit

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,9 @@
 variable "key_name" {
-  default = "Terraform"
+  default = "Terraform_demo"
 }
 
 variable "pvt_key" {
-  default = "/root/.ssh/Terrform.pem"
+  default = "/var/lib/jenkins/Terrform_demo.pem"
 }
 
 variable "us-east-zones" {
@@ -11,5 +11,5 @@ variable "us-east-zones" {
 }
 
 variable "sg-id" {
-  default = "sg-0a6e342b795ebb4b8"
+  default = "sg-075ca3a5467ab6a3e"
 }
